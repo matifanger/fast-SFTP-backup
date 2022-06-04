1fe5665af44476b9f0c184e1490cf3b367016761
Matias Fanger <maattz@outlook.com>
2022-06-03 21:17:31 -0300

    feat(Root): directory and files count sys arg (--count)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, getopt
 import pysftp
 from stat import S_ISDIR, S_ISREG
 from configparser import ConfigParser
@@ -18,9 +18,11 @@ local_path = config["folders"]["local"]
 remote_path = config["folders"]["host"]
 
 dirs = files = 0
+totaldirs = totalfiles = 0
 red = '\033[91m'
 green = '\033[38;2;0;255;0m'
 yellow = '\033[38;2;255;255;0m'
+blue = '\033[94m'
 clear = '\033[0m'
 
 def signal_handler(sig, frame):
@@ -29,7 +31,7 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 def printOut(type = None, entry = ''):
-    global dirs, files
+    global dirs, files, totaldirs, totalfiles
     if type == 'dir':
         dirs += 1
         text = '[ + ] New folder'
@@ -41,9 +43,24 @@ def printOut(type = None, entry = ''):
     elif type == 'dir-exists':
         text = '[ - ] Folder exists'
         color = yellow
+    # Print total dirs and files
+    os.system('clear')
+    print(f'{yellow}Backing up files...')
+    print(f'{blue}Total folders: {totaldirs} - Total files: {totalfiles}')
     print(f'{color}{text} "{entry}" ({dirs} dirs | {files} files)')
 
-def get_r_portable(sftp, remotedir, localdir, preserve_mtime=False):
+def ftp_directory_count(sftp, path):
+    global totaldirs, totalfiles
+    for entry in sftp.listdir(path):
+        remotepath = path + "/" + entry
+        mode = sftp.stat(remotepath).st_mode
+        if S_ISDIR(mode):
+            totaldirs += 1
+            ftp_directory_count(sftp, remotepath)
+        elif S_ISREG(mode):
+            totalfiles += 1
+
+def handler(sftp, remotedir, localdir, preserve=False):
     for entry in sftp.listdir(remotedir):
         remotepath = remotedir + "/" + entry
         localpath = os.path.join(localdir, entry)
@@ -55,16 +72,22 @@ def get_r_portable(sftp, remotedir, localdir, preserve_mtime=False):
             except OSError:     
                 printOut('dir-exists', entry)
                 pass
-            get_r_portable(sftp, remotepath, localpath, preserve_mtime)
+            handler(sftp, remotepath, localpath, preserve)
         elif S_ISREG(mode):
-            sftp.get(remotepath, localpath, preserve_mtime=preserve_mtime)
+            sftp.get(remotepath, localpath, preserve_mtime=preserve)
             printOut('file', entry)
 
 print(f'{yellow}Connecting to {hostname}...')
 try:
     with pysftp.Connection(host=hostname , port=2022, username=username, password=password, cnopts=cnopts) as sftp:
         print(f'{green}Connected to {hostname}')
-        get_r_portable(sftp, remote_path, local_path, preserve_mtime=False)
+
+        if len(sys.argv) > 1:
+            if '--count' in sys.argv:
+                print(f'{yellow}Counting files and folders. This may take a while...')
+                ftp_directory_count(sftp, remote_path)
+
+        handler(sftp, remote_path, local_path, preserve=False)
         print('[ ✓ ] Backup done !')
 except Exception as e:
     print(f'{red}Failed to connect to {hostname}')
